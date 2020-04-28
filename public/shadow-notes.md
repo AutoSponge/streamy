e74eadff2851e11c03546af539c526ab380c6a66
AutoSponge <pgrenier@gmail.com>
2020-04-28 10:41:35 -0400

    add shadow dom example

---
+++ public/shadow-notes.md
@@ -0,0 +1,7 @@
+https://github.com/ampproject/amphtml/blob/master/src/utils/dom-writer.js
+
+https://github.com/jakearchibald/streaming-html
+
+How to gracefully switch to "load more" when streaming not supported.
+1. client will need to use a diff url or diff content-type in req
+1. switch decoder based on type
--- src/take.js
+++ src/take.js
@@ -1,13 +1,13 @@
-module.exports = ifn =>
-  function * (n = Infinity) {
-    let iobj = ifn()
-    let stop = Number(n)
-    let i = 0
-    for (const x of iterable) {
-      if (i === stop) {
-        break
-      }
-      yield x
-      i++
-    }
-  }
+module.exports = (ifn) =>
+	function*(n = Infinity) {
+		let iterable = ifn();
+		let stop = Number(n);
+		let i = 0;
+		for (const x of iterable) {
+			if (i === stop) {
+				break;
+			}
+			yield x;
+			i++;
+		}
+	};
